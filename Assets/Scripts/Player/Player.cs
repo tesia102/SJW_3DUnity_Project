b12f50ef75e009ae92285c989fb1adb137910315
ChangHoon <kangchanghoon12@gmail.com>
2024-02-07 15:01:24 +0900

    기본 세팅

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,214 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    PlayerInputActions inputActions;
+    Rigidbody rigid;
+    Animator animator;
+
+    /// <summary>
+    /// 이동 방향(1 : 전진, -1 : 후진, 0 : 정지)
+    /// </summary>
+    float moveDirection = 0.0f;
+
+    /// <summary>
+    /// 이동 속도
+    /// </summary>
+    public float moveSpeed = 5.0f;
+
+    /// <summary>
+    /// 회전방향(1 : 우회전, -1 : 좌회전, 0 : 정지)
+    /// </summary>
+    float rotateDirection = 0.0f;
+
+    /// <summary>
+    /// 회전 속도
+    /// </summary>
+    public float rotateSpeed = 180.0f;
+
+    /// <summary>
+    /// 애니메이터용 해시값
+    /// </summary>
+    readonly int IsMoveHash = Animator.StringToHash("IsMove");
+    readonly int UseHash = Animator.StringToHash("Use");
+    readonly int DieHash = Animator.StringToHash("Die");
+
+    /// <summary>
+    /// 점프력
+    /// </summary>
+    public float jumpPower = 6.0f;
+
+    /// <summary>
+    /// 점프 중인지 아닌지 나타내는 변수
+    /// </summary>
+    bool isJumping = false;
+
+    /// <summary>
+    /// 점프 쿨 타임
+    /// </summary>
+    public float jumpCoolTime = 1.2f;
+
+    /// <summary>
+    /// 남아있는 쿨타임
+    /// </summary>
+    float jumpCoolRemains = -1.0f;
+
+    /// <summary>
+    /// 점프가 가능한지 확인하는 프로퍼티(점프중이 아니고 쿨타임이 다 지났다.)
+    /// </summary>
+    bool IsJumpAvailable => !isJumping && (jumpCoolRemains < 0.0f);
+
+    /// <summary>
+    /// 플레이어의 생존 여부
+    /// </summary>
+    bool isAlive = true;
+
+    /// <summary>
+    /// 플레이어의 사망을 알리는 델리게이트
+    /// </summary>
+    public Action onDie;
+
+    private void Awake()
+    {
+        //inputActions = new PlayerInputActions();
+        inputActions = new();
+        rigid = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+
+        //ItemUseChecker checker = GetComponentInChildren<ItemUseChecker>();
+        //checker.onItemUse += (interacable) => interacable.Use();
+    }
+
+    private void OnEnable()
+    {
+        inputActions.Player.Enable();
+        inputActions.Player.Move.performed += OnMoveInput;
+        inputActions.Player.Move.canceled += OnMoveInput;
+        inputActions.Player.Jump.performed += OnJumpInput;
+        inputActions.Player.Use.performed += OnUseInput;
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Player.Use.performed -= OnUseInput;
+        inputActions.Player.Jump.performed -= OnJumpInput;
+        inputActions.Player.Move.canceled -= OnMoveInput;
+        inputActions.Player.Move.performed -= OnMoveInput;
+        inputActions.Player.Disable();
+    }
+
+    private void OnMoveInput(InputAction.CallbackContext context)
+    {
+        SetInput(context.ReadValue<Vector2>(), !context.canceled);
+    }
+
+    private void OnJumpInput(InputAction.CallbackContext _)
+    {
+        Jump();
+    }
+
+    private void OnUseInput(InputAction.CallbackContext context)
+    {
+        animator.SetTrigger(UseHash);
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+        Rotate();
+    }
+
+    void Update()
+    {
+        jumpCoolRemains -= Time.deltaTime;
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isJumping = false;
+        }
+    }
+
+    /// <summary>
+    /// 이동 입력 처리용 함수
+    /// </summary>
+    /// <param name="input">입력된 방향</param>
+    /// <param name="isMove">이동 중이면 true, 이동 중이 아니면 false</param>
+    void SetInput(Vector2 input, bool isMove)
+    {
+        rotateDirection = input.x;
+        moveDirection = input.y;
+
+        animator.SetBool(IsMoveHash, isMove);
+    }
+
+    /// <summary>
+    /// 실제 이동 처리 함수(FixedUpdate에서 사용)
+    /// </summary>
+    void Move()
+    {
+        rigid.MovePosition(rigid.position + Time.fixedDeltaTime * moveSpeed * moveDirection * transform.forward);
+    }
+
+    /// <summary>
+    /// 실제 회전 처리 함수(FixedUpdate에서 사용)
+    /// </summary>
+    void Rotate()
+    {
+        // 이번 fixedUpdate에서 추가로 회전할 회전(delta)
+        Quaternion rotate = Quaternion.AngleAxis(Time.fixedDeltaTime * rotateSpeed * rotateDirection, transform.up);
+
+        // 현재 회전에서 rotate만큼 추가로 회전
+        rigid.MoveRotation(rigid.rotation * rotate);
+    }
+
+    /// <summary>
+    /// 실제 점프 처리를 하는 함수
+    /// </summary>
+    void Jump()
+    {
+        if (IsJumpAvailable) // 점프가 가능할 때만 점프
+        {
+            rigid.AddForce(jumpPower * Vector3.up, ForceMode.Impulse);  // 위쪽으로 jumpPower만큼 힘을 더하기
+            jumpCoolRemains = jumpCoolTime; // 쿨타임 초기화
+            isJumping = true;               // 점프했다고 표시
+        }
+    }
+
+    /// <summary>
+    /// 사망 처리용 함수
+    /// </summary>
+    public void Die()
+    {
+        if (isAlive)
+        {
+            Debug.Log("죽었음");
+
+            // 죽는 애니메이션이 나온다.
+            animator.SetTrigger(DieHash);
+
+            // 더 이상 조종이 안되어야 한다.
+            inputActions.Player.Disable();
+
+            // 데굴데굴 구른다.(뒤로 넘어가면서 Y축으로 스핀을 먹는다.)
+            rigid.constraints = RigidbodyConstraints.None;  // 물리 잠금을 전부 해제하기
+
+            Transform head = transform.GetChild(0);
+            rigid.AddForceAtPosition(-transform.forward, head.position, ForceMode.Impulse);
+            rigid.AddTorque(transform.up * 1.5f, ForceMode.Impulse);
+
+            // 죽었다고 신호보내기(onDie 델리게이트 실행)
+            onDie?.Invoke();
+
+            isAlive = false;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# SJW_3DUnity_Project
- 
+# Unity3D_SJW_Project
+ 유니티3D 개인 프로젝트
